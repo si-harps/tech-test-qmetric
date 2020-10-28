f58eb133e1166d3645c6cb42a452ae15cfc3e59e
Simon Harper <simon.harper@uk.dk.com>
2020-10-28 12:40:34 +0000

    Green

--- src/util/calculator.ts
+++ src/util/calculator.ts
@@ -1,5 +1,9 @@
 import { Product } from '../types/product';
 
 export const basketCalulator = (list: Product[]): number => {
+
+  if (!Array.isArray(list))
+    throw new Error('Invalid product');
+    
   return list.reduce( (acc, item) => acc + item.price, 0);
 }
