80c9f4755b5af9982c8d02eb7cd90394f7ebc405
Simon Harper <simon.harper@uk.dk.com>
2020-10-28 12:11:42 +0000

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+import './App.css';
+
+const App: React.FC = () => {
+
+  return (<Fragment>
+    Qmetric test
+  </Fragment>);
+}
+
+export default App;
