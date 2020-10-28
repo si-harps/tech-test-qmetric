d8a697dc04f2a871bbe6ff1ed290af6ea3768651
Simon Harper <simon.harper@uk.dk.com>
2020-10-28 13:23:53 +0000

    Added action types as constants

---
+++ src/constants/ActionTypes.ts
@@ -0,0 +1,7 @@
+// Product action types
+export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+
+// Basket action types
+export const ADD_TO_BASKET = 'ADD_TO_BASKET';
+export const REMOVE_FROM_BASKET = 'REMOVE_FROM_BASKET';
+export const REMOVE_ITEM_FROM_BASKET = 'REMOVE_ITEM_FROM_BASKET';
