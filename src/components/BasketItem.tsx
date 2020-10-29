f0d8e1d2e6d187090a500cfff0c6f2c46b3dd9d1
Simon Harper <simon.harper@uk.dk.com>
2020-10-29 07:45:41 +0000

    Added basket inc/dec-rementer

--- src/components/BasketItem.tsx
+++ src/components/BasketItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AggregatedProduct } from './../types/product';
+import { BasketItemCounter } from './';
 
 interface Props {
   product: AggregatedProduct;
@@ -10,6 +11,7 @@ const BasketItem: React.FC<Props> = ({ product }) => {
   return (
     <li>
       <span className="basket-info product-name">{ product.name }: { product.price.toFixed(2) }</span>
+      <BasketItemCounter product={product} />
       <span className="basket-info product-count pull-right">{ (product.price * product.units).toFixed(2) }</span>
     </li>
   );
